6b5b9f4972a7cbfec7852acf5660d5cbc4eca0e9
Shien <f0928652413@gmail.com>
2022-04-01 15:25:38 +0800

    Initial Commit

---
+++ wordle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordle
+//
+//  Created by 方仕賢 on 2022/4/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
